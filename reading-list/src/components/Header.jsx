a279e1f10b0068570abee85470df67cec7cf07a8
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2025-10-30 01:46:56 -0400

    header section

--- reading-list/src/components/Header.jsx
+++ reading-list/src/components/Header.jsx
@@ -1,7 +1,18 @@
 import '../App.css';
 
 function Header(){
-
+  return(
+      <div className={'header-container'}>
+        <div className={'header-logo'}>
+          <p>📚 Reading List</p>
+        </div>
+        <div className={'header-options'}>
+          <button>All Books</button>
+          <button>Completed</button>
+          <button>To Read</button>
+        </div>
+      </div>
+  )
 }
 
 export default Header;
